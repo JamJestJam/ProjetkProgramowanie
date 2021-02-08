f5b6c0a66408dbb930f22242098f48e63dbd0b01
JamJestJam <marekti012@gmail.com>
2021-02-08 13:19:22 +0100

    Zmiana sposobu wyświetlania listy kategorii

--- ProjektApp/Pages/Products/LeftPanel.xaml.cs
+++ ProjektApp/Pages/Products/LeftPanel.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ProjektApp.Pages.Products {
     /// <summary>
@@ -17,74 +19,34 @@ namespace ProjektApp.Pages.Products {
         }
 
         public void ShowCategory() {
-            var dict = new Dictionary<int, Expander>();
+            var dict = new Dictionary<int, TreeViewItem>();
             var categories = productList.Products.CatergorisRO;
-            Panel.Children.Clear();
+            Panel.Items.Clear();
 
             foreach(var category in categories) {
                 var tmp = CreateConteiner(category.TrueName, category.ID);
                 dict.Add(category.ID, tmp);
 
-                if(category.ParentID == 0) {
-                    Panel.Children.Add(dict[category.ID]);
-                } else {
-                    (dict[(int)category.ParentID].Content as StackPanel)
-                        .Children.Add(dict[category.ID]);
-                }
+                if(category.ParentID == 0)
+                    Panel.Items.Add(dict[category.ID]);
+                else
+                    dict[(int)category.ParentID].Items.Add(dict[category.ID]);
             }
         }
 
-        private Expander CreateConteiner(string name, int ID) {
-            Button button = new Button {
-                Style = this.Resources["MaterialDesignFloatingActionMiniLightButton"] as Style,
-                ToolTip = "Wyszukaj"
-            };
-            button.Click += (object o, RoutedEventArgs e) => ChangeCategory(ID);
-
-            PackIcon packIcon = new PackIcon {
-                Kind = PackIconKind.Magnify
-            };
-            button.Content = packIcon;
-
-            TextBlock textBlock = new TextBlock {
-                Text = name
+        private TreeViewItem CreateConteiner(string name, int ID) {
+            var item = new TreeViewItem();
+            item.Header = name;
+            item.Selected += (object o, RoutedEventArgs e) => { 
+                ChangeCategory(ID); 
+                e.Handled = true; 
             };
 
-            Grid grid = new Grid();
-            var column1 = new ColumnDefinition();
-            var column2 = new ColumnDefinition();
-            var column3 = new ColumnDefinition();
-
-            column1.Width = new GridLength(1, GridUnitType.Auto);
-            column2.Width = new GridLength(1, GridUnitType.Star);
-            column3.Width = new GridLength(1, GridUnitType.Auto);
-
-            grid.ColumnDefinitions.Add(column1);
-            grid.ColumnDefinitions.Add(column2);
-            grid.ColumnDefinitions.Add(column3);
-
-            Grid.SetColumn(button, 2);
-            Grid.SetColumn(textBlock, 0);
-
-            grid.Children.Add(button);
-            grid.Children.Add(textBlock);
-
-            Expander expander = new Expander {
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-                Header = grid
-            };
-
-            StackPanel stackPanel = new StackPanel {
-                Orientation = Orientation.Vertical,
-                Margin = new Thickness(24, 8, 0, 16)
-            };
-
-            expander.Content = stackPanel;
-
-            return expander;
+            return item;
         }
 
         private void ChangeCategory(int id) {
+            Console.WriteLine($"Jestem tu {id}");
             productList.Category = id;
             productList.ShowProducts();
         }
